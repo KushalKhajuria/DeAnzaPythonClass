2596e2355213a5631dc5d8cd9d1a8ddf2bf00870
k00k000 <92127379+k00k000@users.noreply.github.com>
2022-07-05 10:28:39 -0700

    Lab 0 Part 2

---
+++ lab 0 part 2/main.py
@@ -0,0 +1,15 @@
+import random
+
+randomNumber = random.randint(1, 6)
+if randomNumber == 1:
+    print("Roll of dice is 1")
+elif randomNumber == 2:
+    print("Roll of dice is 2")
+elif randomNumber == 3:
+    print("Roll of dice is 3")
+elif randomNumber == 4:
+    print("Roll of dice is 4")
+elif randomNumber == 5:
+    print("Roll of dice is 5")
+else:
+    print("Roll of dice is 6")
